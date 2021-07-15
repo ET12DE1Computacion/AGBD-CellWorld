f8d68c2ce261d9a72ec963e94e1ac5c245cbadb8
AlexisNogueraKurt <ale.capo.9634@gmail.com>
2021-07-15 15:22:20 -0300

    Update dump.sql

--- dump.sql
+++ dump.sql
@@ -7,17 +7,22 @@ START TRANSACTION;
 							(2,'Mediatek');
 	-- Creación de Microprocesadores
 	insert into Microprocesador	(idMicroprocesador,idFabricante,lanzamiento,nucleos,velocidad)
-						values	(1,2,'2019/6/25',8,820);
+						values	(1,2,'2019/6/25',8,820),
+								(2,1,'2015/1/8',8,600);
 	-- Creación de Camaras
 	insert into Camara	(idCamara,idFabricante,megapixeles,apertura)
-				values	(1,1,48,2.0);
+				values	(1,1,48,2.0),
+						(2,1,13,0.28);
 	-- Creación de Sistemas Operativos
 	insert into SistemaOperativo (idSO,nombre,version,lanzamiento)
-						values	(1,'Android 10',2.5,'2019/9/3');
+						values	(1,'Android 10',2.5,'2019/9/3'),
+								(2,'Andoid 5.1.1',1.5,'2016/4/21');
 	-- Creación de Modelos
 	insert into Modelo	(idModelo,idFabricante,idCamara,idMicroprocesador,idSO,modelo,rom,ram,peso,altura,capacidadSD,ancho,profundidad)
-				values	(1,1,1,1,1,'A31',128,6,185,159.3,64,73.1,8.6);
+				values	(1,1,1,1,1,'A31',128,6,185,159.3,64,73.1,8.6),
+						(2,1,2,2,2,'J700M',16,1.5,171,152.2,5.0,7.5,5.5);
 	-- Creación de Celulares
 	insert into Celular	(idModelo)
-				values	(1);
+				values	(1),
+						(2);
 COMMIT;
