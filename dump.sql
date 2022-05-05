8962befaaed4c1ab169ab6e2309d26a58efc445d
silesevelyn <siles.evelyn05@gmail.com>
2022-05-05 12:03:20 -0300

    Agregué Samsung A20

--- dump.sql
+++ dump.sql
@@ -8,21 +8,27 @@ START TRANSACTION;
 	-- Creación de Microprocesadores
 	insert into Microprocesador	(idMicroprocesador,idFabricante,lanzamiento,nucleos,velocidad)
 						values	(1,2,'2019/6/25',8,820),
-								(2,1,'2015/1/8',8,600);
+								(2,1,'2015/3/18',8,600),
+								(3,1,'2019/4/5',8,1.6);
 	-- Creación de Camaras
 	insert into Camara	(idCamara,idFabricante,megapixeles,apertura)
 				values	(1,1,48,2.0),
-						(2,1,13,0.28);
+						(2,1,13,0.28),
+						(3,1,13,1.9);
 	-- Creación de Sistemas Operativos
 	insert into SistemaOperativo (idSO,nombre,version,lanzamiento)
 						values	(1,'Android 10',2.5,'2019/9/3'),
-								(2,'Andoid 5.1.1',1.5,'2016/4/21');
+								(2,'Andoid 5.1.1',1.5,'2016/4/21'),
+								(3,'Android 9.0', 3.1,'2018/8/6' );
+
 	-- Creación de Modelos
 	insert into Modelo	(idModelo,idFabricante,idCamara,idMicroprocesador,idSO,modelo,rom,ram,peso,altura,capacidadSD,ancho,profundidad)
 				values	(1,1,1,1,1,'A31',128,6,185,159.3,64,73.1,8.6),
-						(2,1,2,2,2,'J700M',16,1.5,171,152.2,5.0,7.5,5.5);
+						(2,1,2,2,2,'J700M',16,1.5,171,152.2,5.0,7.5,5.5),
+						(3,1,3,3,3,'SM-A205F',32,3,169,158.4,5.1,74.7,7.8);
 	-- Creación de Celulares
 	insert into Celular	(idModelo)
 				values	(1),
-						(2);
+						(2),
+						(3);
 COMMIT;
